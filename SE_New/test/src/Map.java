07fc64187efd1d96b016ad52b7a54594c76ccd35
Kekschorstviy <117016566+Kekschorstviy@users.noreply.github.com>
2023-12-26 18:16:17 +0100

    Settings/whatever/ try changes
    Here is the Settings menu,there are still
    some minor issues, as lack or to precise
    absence of functionality, but buttons do
    work, so everything should be cool soon.
    
    Much Love,
    Nikolai

---
+++ SE_New/test/src/Map.java
@@ -0,0 +1,23 @@
+public class Map {
+    private int maze1[][];
+    private int maze2[][];
+    private int maze3[][];
+
+//    public Map(){
+//       this.maze1 = new int[][];
+//       this.maze2 = ;
+//       this.maze3 = ;
+//    }
+
+    public int[][] getMaze1() {
+        return maze1;
+    }
+
+    public int[][] getMaze2() {
+        return maze2;
+    }
+
+    public int[][] getMaze3() {
+        return maze3;
+    }
+}
--- SE_New/test/src/Settings.java
+++ SE_New/test/src/Settings.java
@@ -3,7 +3,10 @@ import java.awt.*;
 import java.util.Hashtable;
 
 public class Settings extends JFrame {
-    private MapSelectionListener listener;
+    private final MapSelectionListener listener;
+    protected boolean map1;
+    protected boolean map2;
+    protected boolean map3;
 
     public Settings(MapSelectionListener listener) {
         this.listener = listener;
@@ -17,19 +20,13 @@ public class Settings extends JFrame {
         setLayout(new FlowLayout());
 
         JButton map1Button = new JButton("Map 1");
-        map1Button.addActionListener((e) -> {
-            this.listener.onMapSelected(1);
-        });
+        map1Button.addActionListener((e) -> this.listener.onMapSelected(1));
 
         JButton map2Button = new JButton("Map 2");
-        map2Button.addActionListener((e) -> {
-            this.listener.onMapSelected(2);
-        });
+        map2Button.addActionListener((e) -> this.listener.onMapSelected(2));
 
         JButton map3Button = new JButton("Map 3");
-        map3Button.addActionListener((e) -> {
-            this.listener.onMapSelected(3);
-        });
+        map3Button.addActionListener((e) -> this.listener.onMapSelected(3));
 
         this.add(map1Button);
         this.add(map2Button);
@@ -51,7 +48,7 @@ public class Settings extends JFrame {
         JPanel settingsPanel = new JPanel();
         settingsPanel.setLayout(new GridLayout(3, 2, 10, 10));
 
-        // Add components for Map option
+        // Add components for a Map option
         JLabel mapLabel = new JLabel("Map:");
 
         //This stuff adjusts the border between the screen edge and text (Gaping)
@@ -60,7 +57,7 @@ public class Settings extends JFrame {
         settingsPanel.add(mapLabel);
         settingsPanel.add(mapOptions);
 
-        // Add components for Pacman Speed option
+        // Add components for a Pacman Speed option
         JLabel pacmanSpeedLabel = new JLabel("Pacman Speed:");
 
         //This stuff adjusts the border between the screen edge and text (Gaping)
@@ -70,7 +67,7 @@ public class Settings extends JFrame {
         settingsPanel.add(pacmanSpeedLabel);
         settingsPanel.add(pacmanSpeedSlider);
 
-        // Add components for Ghost Speed option
+        // Add components for a Ghost Speed option
         JLabel ghostSpeedLabel = new JLabel("Ghost Speed:");
 
         //This stuff adjusts the border between the screen edge and text (Gaping)
@@ -88,6 +85,7 @@ public class Settings extends JFrame {
             int newPacmanSpeed = ((JSlider) e.getSource()).getValue();
             System.out.println("New Pacman Speed: " + newPacmanSpeed);
             // Logic how to actually change is to be implemented
+//            Pacman.getInstance(). // change speed
         });
 
         // Update Ghost speed
@@ -95,15 +93,20 @@ public class Settings extends JFrame {
             int newGhostSpeed = ((JSlider) e.getSource()).getValue();
             System.out.println("New Ghost Speed: " + newGhostSpeed);
             // Logic how to actually change is to be implemented
+            //same here
         });
 
         // Update the map
         mapOptions.addActionListener(e -> {
             String selectedMap = (String) mapOptions.getSelectedItem();
             System.out.println("Selected Map: " + selectedMap);
-            // Logic how to actually change is to be implemented
-            // Proposal:
-            // listener.onMapSelected(mapNumber);
+
+            if ((int)mapOptions.getSelectedItem() == 1)
+                map1 = true;
+            if ((int)mapOptions.getSelectedItem() == 2)
+                map2 = true;
+            if ((int)mapOptions.getSelectedItem() == 3)
+                map3 = true;
         });
     }
 
@@ -117,6 +120,18 @@ public class Settings extends JFrame {
             labelTable.put(i, new JLabel(Integer.toString(i)));
         }
         slider.setLabelTable(labelTable);
+    }
+
+    public boolean isMap1() {
+        return map1;
+    }
+
+    public boolean isMap2() {
+        return map2;
+    }
+
+    public boolean isMap3() {
+        return map3;
     }
 }
 
