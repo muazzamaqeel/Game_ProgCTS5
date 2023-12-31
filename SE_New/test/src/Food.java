248d36fcf837820b49114e17cedbbadb3be07ba4
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2023-12-31 20:33:07 +0000

    Multiple Things Fixed
    Flickering of Game (Fixed)
    Food replaced with actual pictures

--- SE_New/test/src/Food.java
+++ SE_New/test/src/Food.java
@@ -1,104 +1,80 @@
-import javax.swing.*;
+import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Food {
-    private List<Point> foodPositions;
-    private Board board;
+    private final Board board;
     private int score;
-    private float glowRadius = 10; // Initial glow radius
-    private boolean increasingGlow = true;
+    private BufferedImage foodImage1;
+    private BufferedImage foodImage2;
+    private List<FoodItem> foodItems; // This is the list we will use
+
+    // Adjust these constants to scale the food size
+    private static final int FOOD_WIDTH = Board.GRID_WIDTH / 2;
+    private static final int FOOD_HEIGHT = Board.GRID_HEIGHT / 2;
 
     public Food(Board board) {
         this.board = board;
-        initializeFoodPositions();
         score = 0;
+        loadImages();
+        initializeFoodPositions();
+    }
+
+    private static class FoodItem {
+        Point position;
+        BufferedImage image;
+
+        FoodItem(Point position, BufferedImage image) {
+            this.position = position;
+            this.image = image;
+        }
+    }
+
+    private void loadImages() {
+        try {
+            foodImage1 = ImageIO.read(Objects.requireNonNull(getClass().getResource("/game/images/food/food1.png")));
+            foodImage2 = ImageIO.read(Objects.requireNonNull(getClass().getResource("/game/images/food/food2.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the error by providing feedback or exiting
+        }
     }
 
     private void initializeFoodPositions() {
-        foodPositions = new ArrayList<>();
+        foodItems = new ArrayList<>();
         int[][] grid = board.getGrid();
         for (int row = 0; row < Board.TILE_NUMBER; row++) {
             for (int col = 0; col < Board.TILE_NUMBER; col++) {
                 if (grid[row][col] == 0) {
                     int xCenter = (col * Board.GRID_WIDTH) + (Board.GRID_WIDTH / 2);
                     int yCenter = (row * Board.GRID_HEIGHT) + (Board.GRID_HEIGHT / 2);
-                    foodPositions.add(new Point(xCenter, yCenter));
+                    BufferedImage foodImage = (row + col) % 2 == 0 ? foodImage1 : foodImage2;
+                    foodItems.add(new FoodItem(new Point(xCenter, yCenter), foodImage));
                 }
             }
         }
     }
 
     public void draw(Graphics2D g2) {
-        updateGlowEffect();
-
-        for (Point point : foodPositions) {
-            drawGlowingBall(g2, point, glowRadius);
+        for (FoodItem foodItem : foodItems) {
+            int drawX = foodItem.position.x - FOOD_WIDTH / 2;
+            int drawY = foodItem.position.y - FOOD_HEIGHT / 2;
+            g2.drawImage(foodItem.image, drawX, drawY, FOOD_WIDTH, FOOD_HEIGHT, null);
         }
     }
-
-    private void drawGlowingBall(Graphics2D g2, Point center, float radius) {
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        RadialGradientPaint paint = new RadialGradientPaint(
-                new Point2D.Float(center.x, center.y),
-                radius,
-                new float[] {0.0f, 0.5f, 1.0f},
-                new Color[] {
-                        new Color(255, 255, 224, 255),  // bright center
-                        new Color(255, 215, 0, 230),     // gold middle
-                        new Color(255, 215, 0, 180)      // darker edge
-                }
-        );
-        g2.setPaint(paint);
-        g2.fillOval((int) (center.x - radius), (int) (center.y - radius), (int) (radius * 2), (int) (radius * 2));
-
-        paint = new RadialGradientPaint(
-                new Point2D.Float(center.x - radius * 0.2f, center.y - radius * 0.2f),
-                radius * 0.5f,
-                new float[] {0.0f, 1.0f},
-                new Color[] {new Color(255, 255, 255, 200), new Color(255, 255, 255, 0)}
-        );
-        g2.setPaint(paint);
-        g2.fillOval((int) (center.x - radius), (int) (center.y - radius), (int) (radius * 2), (int) (radius * 2));
-        paint = new RadialGradientPaint(
-                center,
-                radius * 1.5f,
-                new float[] {0.0f, 1.0f},
-                new Color[] {new Color(255, 255, 100, 50), new Color(255, 255, 0, 0)}
-        );
-        g2.setPaint(paint);
-        g2.fillOval((int) (center.x - radius * 1.5f), (int) (center.y - radius * 1.5f), (int) (radius * 3f), (int) (radius * 3f));
-    }
-
-
-    private void updateGlowEffect() {
-        if (increasingGlow) {
-            glowRadius += 0.2;
-            if (glowRadius > 15) {
-                increasingGlow = false;
-            }
-        } else {
-            glowRadius -= 0.2;
-            if (glowRadius < 10) {
-                increasingGlow = true;
-            }
-        }
-    }
-
-    // Rest of your class...
-
 
     public void checkCollisionWithPacman(Pacman pacman) {
         Rectangle2D pacmanBounds = new Rectangle2D.Double(pacman.getX(), pacman.getY(), Pacman.PLAYER_SIZE, Pacman.PLAYER_SIZE);
-        foodPositions.removeIf(point -> {
-            Rectangle2D foodBounds = new Rectangle2D.Double(point.x, point.y, Board.GRID_WIDTH, Board.GRID_HEIGHT);
+        foodItems.removeIf(foodItem -> {
+            Rectangle2D foodBounds = new Rectangle2D.Double(foodItem.position.x - (double) FOOD_WIDTH / 2, foodItem.position.y - (double) FOOD_HEIGHT / 2, FOOD_WIDTH, FOOD_HEIGHT);
             if (pacmanBounds.intersects(foodBounds)) {
-                score += 10; // Increment score for each food eaten
-                return true; // Remove the food from the list
+                score += 1;
+                return true;
             }
             return false;
         });
